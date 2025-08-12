8336853e9819995249fa910648fc03d23a85f8da
William Park <galaxiapark07@gmail.com>
2025-08-13 01:56:12 +1000

    Remove build directory from Git tracking

--- build/perf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/williampark/Ask-for-Sharing-SLF/perf
-# Build directory: /Users/williampark/Ask-for-Sharing-SLF/build/perf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/CMakeFiles/tools_unittest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tools_unittest.
-# This may be replaced when dependencies are built.
